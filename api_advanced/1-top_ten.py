ff1371ccbc1fd72a4aa7abfb1f0dc21cae9bd41b
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-07-15 15:05:06 +0200

    Refactor top_ten function to improve readability and add parameters for request

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,25 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozzilla/5.0'}
+    params = {'limit': 10}
 
-    if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
+    response = requests.get(
+        reddit_url, headers=headers, params=params, allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+    
+    try:
+        data =response.json().get('data', {}).get('children', [])
+        if not data:
+            print(None)
+            return
+        for post in data[:10]:
             print(post['data']['title'])
-    else:
+
+    except Exception:
         print(None)
